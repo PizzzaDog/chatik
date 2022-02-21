7babfbd4300402d168417b29c52abc426e118c68
PizzzaDog <7883494@gmail.com>
2022-02-21 22:52:25 +0300

    master committ

--- src/main/java/by/overone/NewClass.java
+++ src/main/java/by/overone/NewClass.java
@@ -3,5 +3,6 @@ package by.overone;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("MFCKRS");
+        System.out.println("master branch changes");
     }
 }
